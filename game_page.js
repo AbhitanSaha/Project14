d604fc062ff30fec93fa65cd5081e40c875a8c9e
AbhitanSaha <72934030+AbhitanSaha@users.noreply.github.com>
2020-12-14 06:59:49 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -19,6 +19,41 @@ function send(){
     chack_button="<br><br><button class='btn btn-info'onclick='check()' >Check</button>";
     row=question_word+input_box+chack_button;
     document.getElementById("output").innerHTML=row;
-    document.getElementById("num1").innerHTML="";
-    document.getElementById("num2").innerHTML="";
+    document.getElementById("num1").value="";
+    document.getElementById("num2").value="";
+}
+question_turn="player1";
+answer_turn="player2";
+function check(){
+    get_word2=document.getElementById("input_check_box").value;
+    word2=get_word2.toLowerCase();
+    console.log("answer in lower case="+word);
+
+    if(actualans==word2){
+        if(answer_turn=="player1"){
+            player1_score=player1_score+1;
+            document.getElementById("player1_score").innerHTML=player1_score;
+        }
+        else{
+            player2_score=player2_score+1;
+            document.getElementById("player2_score").innerHTML=player2_score;
+        }
+        if(answer_turn=="player2"){
+            answer_turn="player1";
+            document.getElementById("player_answer").innerHTML="Answer-"+player1_name;
+        }
+        else{
+            answer_turn="player2";
+            document.getElementById("player_answer").innerHTML="Answer-"+player2_name;
+        }
+        if(question_turn=="player1"){
+        question_turn="player2";
+        document.getElementById("player_question").innerHTML="Question-"+player2_name;
+        }
+        else{
+            question_turn="player1";   
+            document.getElementById("player_question").innerHTML="Question-"+player1_name;
+        }
+        document.getElementById("output").innerHTML="";
+    }
 }
